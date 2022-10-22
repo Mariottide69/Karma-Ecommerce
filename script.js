d263b97588f13156943e4eaaeb19c40753f82009
Mariottide69 <mario.valenti12@gmail.com>
2022-10-22 18:54:00 +0200

    navbar completed

---
+++ script.js
@@ -0,0 +1,20 @@
+const navbarWrapper = document.querySelector('#navbarWrapper');
+const btnToggleSearch = document.querySelector('#btnToggleSearch');
+const searchWrapper = document.querySelector('#searchWrapper');
+const btnCloseSearch = document.querySelector('#btnCloseSearch');
+
+window.addEventListener('scroll', () => {
+   if (window.scrollY > 40){
+      navbarWrapper.classList.add('scrolled')
+   } else {
+      navbarWrapper.classList.remove('scrolled')
+   }
+})
+
+btnToggleSearch.addEventListener('click', () => {
+   searchWrapper.classList.toggle('closed')
+})
+
+btnCloseSearch.addEventListener('click', () => {
+   searchWrapper.classList.toggle('closed')
+})
