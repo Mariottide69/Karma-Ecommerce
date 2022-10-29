0e171aff584013a0eef9af449dfc1493bced8710
Mariottide69 <mario.valenti12@gmail.com>
2022-10-29 16:44:19 +0200

    carousel

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ btnToggleSearch.addEventListener('click', () => {
 btnCloseSearch.addEventListener('click', () => {
    searchWrapper.classList.toggle('closed')
 })
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+   slidesPerView: 1,
+   spaceBetween: 30,
+   breakpoints: {
+      640: {
+         slidesPerView: 1,
+         spaceBetween: 10,
+      },
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 20,
+      },
+      1024: {
+         slidesPerView: 3,
+         spaceBetween: 30,
+      },
+   },
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
