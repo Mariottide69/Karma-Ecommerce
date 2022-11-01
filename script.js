f7ab2fe63cf9ee731f15682861c1b8b98e352c4b
Mariottide69 <mario.valenti12@gmail.com>
2022-11-01 19:25:59 +0100

    modifiche varie

--- script.js
+++ script.js
@@ -52,3 +52,37 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
    },
 });
+
+//! counter
+
+let counterDaysLeft = document.querySelector('#counterDaysLeft');
+let counterHoursLeft = document.querySelector('#counterHoursLeft');
+let counterMinsLeft = document.querySelector('#counterMinsLeft');
+let counterSecsLeft = document.querySelector('#counterSecsLeft');
+
+function formatCounter(num) {
+   if (num < 10){
+      return `0${num}`
+   } else {
+      return num;
+   }
+}
+
+
+let counterInterval = setInterval(() => {
+  let today = Date.now();
+  let offerExpire = Date.parse("05/16/2023");
+  let secondsToExpireDate = Math.floor((offerExpire - today) / 1000);
+
+  let daysLeft = Math.floor(secondsToExpireDate / 24 / 60 / 60);
+  let hoursLeftInSec = Math.floor(secondsToExpireDate - daysLeft * 86400);
+  let hoursLeft = Math.floor(hoursLeftInSec / 3600);
+  let minsLeftInSec = Math.floor(hoursLeftInSec - hoursLeft * 3600);
+  let minsLeft = Math.floor(minsLeftInSec / 60);
+  let secondsLeft = Math.floor(secondsToExpireDate % 60);
+  
+  counterDaysLeft.innerHTML = formatCounter(daysLeft);
+  counterHoursLeft.innerHTML = formatCounter(hoursLeft);
+  counterMinsLeft.innerHTML = formatCounter(minsLeft);
+  counterSecsLeft.innerHTML = formatCounter(secondsLeft);
+}, 1000);
